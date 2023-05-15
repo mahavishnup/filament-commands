7aaf77971000c6976550692a459d436beb8a3e18
mahavishnup <selvamvishnu25@gmail.com>
2023-05-15 10:55:19 +0530

    filamentsheild install

--- src/Pages/Artisan.php
+++ src/Pages/Artisan.php
@@ -2,21 +2,24 @@
 
 namespace io3x1\FilamentCommands\Pages;
 
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 use io3x1\FilamentCommands\Http\Controllers\GuiController;
 
 class Artisan extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationIcon = 'heroicon-o-code';
 
     protected static string $view = 'gui::index';
 
     protected static ?string $navigationGroup = 'Settings';
 
-    protected static function shouldRegisterNavigation(): bool
-    {
-        return static::hasCommands();
-    }
+//    protected static function shouldRegisterNavigation(): bool
+//    {
+//        return static::hasCommands();
+//    }
 
     public function mount(): void
     {
